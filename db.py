31c3ca1e12514eb35c4ae8eccc41e9d875cf75ac
aleksandrO3 <xboxonegamer@yandex.ru>
2022-07-19 01:06:39 +0300

    fixed request issues, same codes for same links

--- db.py
+++ db.py
@@ -1,42 +1,75 @@
 from redis import Redis  
 from string import ascii_letters
 from random import choice
+from json import load, dumps
 
 r = Redis(host='localhost', port=6379, db=0)
 url = "http://127.0.0.1:5000/"
 
-def setLink(link, time):
-    notFoundKey = True
 
-    while notFoundKey:
-        code = ''.join({choice(ascii_letters) for x in range(5)})
-        if not r.exists(code):
-            notFoundKey = False
-
+def setLink(link, time):  
+    code = generate_code(link)
     time = calculate_time(time)
     r.set(code, link, time)
     return {'encoded_link': url + code}
 
-def getLink(link):
+
+def getLink(link):  
     if r.exists(link):
         return {'decoded_link': str(r.get(link)).strip('b')}  
     else:
         return 'No such key'
 
-def calculate_time(time):
-    try: # here we can use arrow or delorean libraries functions instead
-        time = list(map(lambda x: int(x), time.split(':')))
-        multiplier = 3600
-        
-        newTime = time[0] * 86400  
-        
-        for i in time[1:]:
-            newTime +=  i * multiplier
-            multiplier = int(multiplier / 60)
 
-        return newTime
+def calculate_time(time): 
+    try: 
+        return int(time) * 86400
     except ValueError:
-        return 600
+        return 86400
+
+def generate_code(link): 
+    notFoundKey = True
+    valueExist = isValueInDB(link)
+
+    if not valueExist:
+        while notFoundKey:
+            code = ''.join({choice(ascii_letters) for x in range(5)})
+            if not r.exists(code):
+                notFoundKey = False
+
+        addPairToDB(link, code)
+
+        return code 
+    else: 
+        return valueExist
     
+
+def isValueInDB(value):
+    with open('base.json', 'r') as f:
+        base = load(f)
+
+    if value in base.keys():
+        if not r.exists(base[value]): # saved in json but deleted from db
+            del base[value]
+            
+            with open('base.json', 'w') as f: 
+                base = dumps(base)
+                f.write(base)
+            
+            return False  
+        else:
+            return base[value]
+    else:
+        return False 
+
+
+def addPairToDB(key, value):
+    with open('base.json', 'r') as f:
+        base = load(f)
     
+    base[key] = value
+
+    with open('base.json', 'w') as f:
+        base = dumps(base)        
+        f.write(base)
 
--- run.py
+++ run.py
@@ -1,5 +1,19 @@
-from __init__ import app
-import routing
+from flask import Flask, request
+from db import *
 
-app.run(debug = True)
+app = Flask(__name__)
+
+@app.route('/', methods=["POST"])
+def processingPost():
+    data = request.get_json()
+    return setLink(data['link'], data['time'])
+  
+    
+@app.route('/<code>', methods=['GET'])
+def showLink(code):
+    return getLink(code)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
